fbe8fa21c5ada6d23b06f52c7edfb950acb742e3
Sahana J <jaikumarsahana04@gmail.com>
2020-10-17 01:24:39 +0530

    Simple ReactJS example

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+
+function App() {
+  return (
+    
+    <div className="heading">
+      <div class="name"><h1>Sahana J</h1></div>
+        <div class="row">
+          <img src="https://www.freepnglogos.com/uploads/google-favicon-logo-20.png" alt="Google icon" width={200} height={200} />
+          <img src="https://www.freepnglogos.com/uploads/microsoft-window-logo-emblem-0.png" alt="Microsoft logo" width={200} height={200}/>
+        </div>
+
+        <div><p>They Both Are Competitors</p></div>
+      </div>  
+     
+     
+    
+  );
+}
+
+export default App;
